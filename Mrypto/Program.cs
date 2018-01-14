f51ea5b185c4aa994abca91c0f41aed2cf01b248
mmatthew <takeknock@gmail.com>
2018-01-14 18:04:20 +0900

    add procedure of RSA KeyGen, Encryption and Decryption.

--- Mrypto/Program.cs
+++ Mrypto/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Numerics;
 
 namespace Mrypto
 {
@@ -10,6 +11,23 @@ namespace Mrypto
     {
         static void Main(string[] args)
         {
+            //KeyGen
+
+            RSA.KeyGen generator = new RSA.KeyGen();
+            generator.initialize();
+            Tuple<RSA.PublicKey, RSA.SecretKey> keys = generator.createKeys();
+            Console.WriteLine(keys.ToString());
+
+            //Encryption
+            //1. calculate c = m^e mod N
+
+            //Decription
+            //1. m' = c^d mod N
+
+
+
+            //Check m==m'
+
         }
     }
 }
