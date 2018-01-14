83450532ce53f6e1b90c71cae4bdffd7e15970fc
mmatthew <takeknock@gmail.com>
2018-01-14 18:45:28 +0900

    add full procedure of RSA encryption

--- Mrypto/Program.cs
+++ Mrypto/Program.cs
@@ -16,18 +16,26 @@ namespace Mrypto
             RSA.KeyGen generator = new RSA.KeyGen();
             generator.initialize();
             Tuple<RSA.PublicKey, RSA.SecretKey> keys = generator.createKeys();
+            RSA.PublicKey publicKey = keys.Item1;
+            RSA.SecretKey secretKey = keys.Item2;
             Console.WriteLine(keys.ToString());
 
             //Encryption
             //1. calculate c = m^e mod N
+            RSA.Encryptor enc = new RSA.Encryptor();
+            BigInteger message = 12;
+            BigInteger encryptedMessage = 
+                enc.encrypt(message, publicKey);
 
             //Decription
             //1. m' = c^d mod N
+            RSA.Decryptor dec = new RSA.Decryptor();
 
-
+            BigInteger decryptedMessage =
+                dec.decrypt(encryptedMessage, publicKey, secretKey);
 
             //Check m==m'
-
+            Console.WriteLine("m==m': " + (encryptedMessage == decryptedMessage).ToString());
         }
     }
 }
